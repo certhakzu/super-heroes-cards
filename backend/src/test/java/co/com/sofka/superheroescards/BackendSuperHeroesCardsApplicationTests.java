157c9124a9b5eda50230359b6ff2442e23d5b7d2
certhakzu <pealveco@yahoo.com>
2022-07-20 12:49:29 -0500

    estructuras de backend y frontend creadas

---
+++ backend/src/test/java/co/com/sofka/superheroescards/BackendSuperHeroesCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.superheroescards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSuperHeroesCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
