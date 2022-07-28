6f5bc483bfe443fedcf0b958aadb7e74f53d3244
certhakzu <pealveco@yahoo.com>
2022-07-28 01:40:39 -0500

    primera conexion entre front y back

--- frontend/src/app/TarjetasService/tarjetas-service.service.ts
+++ frontend/src/app/TarjetasService/tarjetas-service.service.ts
@@ -6,31 +6,31 @@ import  {Tarjeta} from '../models/Tarjetas'
   providedIn: 'root'
 })
 export class TarjetasServiceService {
-  API_URI='http://localhost:3000/api';
+  //API_URI='http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
   getTarjetas(){
-    return this.http.get(`${this.API_URI}/tarjetas`);
+    return this.http.get(`api/listartarjetas`);
 
   }
   getTarjeta(id:string){
-    return this.http.get(`${this.API_URI}/tarjeta/${id}`);
+    return this.http.get(`api/tarjeta/${id}`);
 
   }
 
   deleteTarjeta(id:string){
-    return this.http.delete(`${this.API_URI}/tarjetas/${id}`);
+    return this.http.delete(`api/tarjetas/${id}`);
 
 
   }
-  
+
   saveTarjeta(tarjeta:Tarjeta){
-    return this.http.post(`${this.API_URI}/tarjeta`,tarjeta);
+    return this.http.post(`api/tarjeta`,tarjeta);
 
   }
   updateTarjeta(id: string|number,updateTarjeta:Tarjeta) {
-    return this.http.put(`${this.API_URI}/Tarjetas/${id}`,updateTarjeta);
+    return this.http.put(`api/Tarjetas/${id}`,updateTarjeta);
 
   }
 
