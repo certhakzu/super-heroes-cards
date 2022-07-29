476a4b6eba740e25e4a55c131b31bf960d025c09
certhakzu <pealveco@yahoo.com>
2022-07-29 13:55:49 -0500

    No funciona el back: revisar la comunicacino entre los paquetes

--- backend/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/mazo/MazoMongoDBRepository.java
+++
@@ -1,8 +0,0 @@
-package co.com.sofka.mongo.mazo;
-
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
-
-public interface MazoMongoDBRepository extends ReactiveMongoRepository<MazoDocument, String>, ReactiveQueryByExampleExecutor<MazoDocument> {
-}
